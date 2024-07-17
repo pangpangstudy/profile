66302ab132a87dcae2592728e447b35dffd7f7c9
Zzh <3169427572@qq.com>
2024-07-17 22:41:37 +0800

    fixbug

--- src/components/skill/index.tsx
+++ src/components/skill/index.tsx
@@ -1,12 +1,12 @@
 import { skillsData } from "@/lib/utils";
 import gsap from "gsap/all";
-import { useEffect, useRef } from "react";
-
+import { useLayoutEffect, useRef } from "react";
+import bg from "../../assets/images/fengjing.jpg";
 export default function Skills() {
   const skillRef = useRef<HTMLDivElement>(null);
   const skillRefL = useRef<HTMLDivElement>(null);
   const skillRefR = useRef<HTMLDivElement>(null);
-  useEffect(() => {
+  useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       gsap.to(skillRefL.current?.querySelectorAll("ul")!, {
         x: "100%",
@@ -20,7 +20,7 @@ export default function Skills() {
         duration: 10,
         repeat: -1,
       });
-    }, skillRef);
+    });
     return () => ctx.revert();
   }, []);
 
@@ -32,10 +32,13 @@ export default function Skills() {
       <h1 className="uppercase  text-center absolute top-0 py-10 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500 text-[5vw] z-10 ">
         my skills
       </h1>
-
+      <div
+        className="w-full h-full bg-white bg-cover bg-center "
+        style={{ backgroundImage: `url(${bg})` }}
+      ></div>
       <div
         ref={skillRefL}
-        className="flex items-center h-1/6 md:h-1/4  overflow-hidden rotate-[45deg] md:rotate-[35deg] lg:rotate-[25deg]"
+        className="absolute flex items-center h-[10%] md:h-[12%]  overflow-hidden rotate-[45deg] md:rotate-[35deg] lg:rotate-[25deg]"
       >
         <ul className="flex w-[200%] h-full items-center gap-8 px-4 py-4 bg-yellow-400 ">
           {skillsData.slice(0, 10).map((skill, index) => {
@@ -91,7 +94,7 @@ export default function Skills() {
       </div>
       <div
         ref={skillRefR}
-        className="flex items-center h-1/6 md:h-1/4  overflow-hidden rotate-[-45deg] md:rotate-[-35deg] lg:rotate-[-25deg] absolute"
+        className="flex items-center h-[10%] md:h-[12%]  overflow-hidden rotate-[-45deg] md:rotate-[-35deg] lg:rotate-[-25deg] absolute"
       >
         <ul className="flex w-[200%] h-full items-center gap-8 px-4 py-4 bg-yellow-400 ">
           {skillsData.slice(10).map((skill, index) => {
@@ -148,3 +151,4 @@ export default function Skills() {
     </section>
   );
 }
+// git reset --hard a874cf210aab84ec3b8f87dddbcfd67d28e74e44
